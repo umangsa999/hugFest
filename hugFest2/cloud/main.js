5217dc67347ef9bcc7a505ed2b68358b98683497
Ryan Zhou <ryanzhou7@gmail.com>
2015-06-22 13:49:01 -0700

    Uploading change

--- hugFest2/cloud/main.js
+++ hugFest2/cloud/main.js
@@ -5,7 +5,3 @@ Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
