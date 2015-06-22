4091ae0a54b7480dbf161288d09a14259cccd019
Chris Lee <chrisl@usc.edu>
2015-06-22 14:03:12 -0700

    Moved to a proper place, i.e. only on success.

--- hugFest2/public/js/login.js
+++ hugFest2/public/js/login.js
@@ -8,11 +8,11 @@ function login(){
 	user.signUp(null, {
 	  success: function(user) {
 	    // Hooray! Let them use the app now.
+	    location.href='home.html';
 	  },
 	  error: function(user, error) {
 	    // Show the error message somewhere and let the user try again.
 	    alert("Error: " + error.code + " " + error.message);
 	  }
 	});
-	location.href='home.html';
 };
