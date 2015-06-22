00a013a516aa8cda8f3b1e3eafc30c59fcd6c46f
Ryan Zhou <ryanzhou7@gmail.com>
2015-06-22 16:42:49 -0700

    working code that gets array of users

--- hugFest2/public/js/friends.js
+++ hugFest2/public/js/friends.js
@@ -1,14 +1,14 @@
-arse.initialize("RNWZPB7mbzkA9YL7pWy2TW4GW5MzeLvfxjonPoEl", "t8uqZXplrRlSJ3qp5ILEJQPWjQFPM7zsCDvmw3qe");
+Parse.initialize("RNWZPB7mbzkA9YL7pWy2TW4GW5MzeLvfxjonPoEl", "t8uqZXplrRlSJ3qp5ILEJQPWjQFPM7zsCDvmw3qe");
 function addFriend(){
 	var userName = prompt("Enter your friends name you lonely person");
 
 	//Call cloud code newFriend
-	Parse.Cloud.run( 'hello', {user: userName}, {
+	Parse.Cloud.run( 'findFriend', {user: userName}, {
 		success: function(code){
-			alert("Success");
+			//alert(code.message + "from " + code.length);
 		},
 		error: function(error){
-			alert("Error");
+			alert("Error friend not found");
 		}
 	});
 
--- hugFest2/public/js/login.js
+++ hugFest2/public/js/login.js
@@ -1,4 +1,3 @@
-
 Parse.initialize("RNWZPB7mbzkA9YL7pWy2TW4GW5MzeLvfxjonPoEl", "t8uqZXplrRlSJ3qp5ILEJQPWjQFPM7zsCDvmw3qe");
 function login(){
 	var user = new Parse.User();
