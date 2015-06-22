5d693ccfce04c594266dfeee5eb2c8949d1659a5
Ryan Zhou <ryanzhou7@gmail.com>
2015-06-22 15:53:02 -0700

    Correct login keys

--- hugFest2/public/js/friends.js
+++ hugFest2/public/js/friends.js
@@ -1,17 +1,15 @@
-Parse.initialize("yingEebCnDBZKPt7WfM6Hn7Jy6SEjtU3l3dHsWNj", "AdmT1bexKKsNvr8KU3wIGAtBafoKcfPolcmStnS4");
+arse.initialize("RNWZPB7mbzkA9YL7pWy2TW4GW5MzeLvfxjonPoEl", "t8uqZXplrRlSJ3qp5ILEJQPWjQFPM7zsCDvmw3qe");
 function addFriend(){
 	var userName = prompt("Enter your friends name you lonely person");
-
-
-	
 
 	//Call cloud code newFriend
 	Parse.Cloud.run( 'hello', {user: userName}, {
 		success: function(code){
-			alert(code.message);
+			alert("Success");
 		},
 		error: function(error){
-			alert(error.message);
+			alert("Error");
 		}
 	});
+
 }
