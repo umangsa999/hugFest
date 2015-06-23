7a7828947c2f827aaced133b7121867a6c19283f
Ryan Zhou <ryanzhou7@gmail.com>
2015-06-22 18:57:03 -0700

    Any changes

--- hugFest2/public/js/login.js
+++ hugFest2/public/js/login.js
@@ -48,15 +48,6 @@ function statusChangeCallback(response) {
 	    });
 	  }
 
-	  window.fbAsyncInit = function() {
-	  FB.init({
-	    appId      : '715839931872390',
-	    cookie     : true,  // enable cookies to allow the server to access 
-	                        // the session
-	    xfbml      : true,  // parse social plugins on this page
-	    version    : 'v2.2' // use version 2.2
-	  });
-
 	  // Now that we've initialized the JavaScript SDK, we call 
 	  // FB.getLoginStatus().  This function gets the state of the
 	  // person visiting this page and can return one of three states to
@@ -69,26 +60,15 @@ function statusChangeCallback(response) {
 	  //
 	  // These three cases are handled in the callback function.
 
-	  FB.getLoginStatus(function(response) {
-	    alert("Checking if logged in already");
-	    statusChangeCallback(response);
-	    if( response.status === 'connected'){
-	      alert("Logged in!");
-	    }
-	  });
 
+	  // FB.getLoginStatus(function(response) {
+		 //    alert("Checking if logged in already");
+		 //    statusChangeCallback(response);
+		 //    if( response.status === 'connected'){
+		 //      alert("Logged in!");
+		 //    }
+	  // });
 
-	  };
-
-	  // Load the SDK asynchronously
-
-	  (function(d, s, id) {
-	    var js, fjs = d.getElementsByTagName(s)[0];
-	    if (d.getElementById(id)) return;
-	    js = d.createElement(s); js.id = id;
-	    js.src = "//connect.facebook.net/en_US/sdk.js";
-	    fjs.parentNode.insertBefore(js, fjs);
-	  }(document, 'script', 'facebook-jssdk'));
 
 	  // Here we run a very simple test of the Graph API after login is
 	  // successful.  See statusChangeCallback() for when this call is made.
