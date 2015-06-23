a7c03e504b4d25cd192bbfe03dd9baaba8342099
Ryan Zhou <ryanzhou7@gmail.com>
2015-06-23 14:23:35 -0700

    Added another cloud folder, was ggoing to redo project but refreshing cookies made project work

---
+++ hugFest3/public/js/login.js
@@ -0,0 +1,90 @@
+
+function login(){
+	
+	alert("hello there");
+	var TestObject = Parse.Object.extend("TestObject");
+	var testObject = new TestObject();
+	testObject.save({foo: "bar"}).then(function(object) {
+	  alert("yay! it worked");
+	});
+
+	var user = new Parse.User();
+	user.set("username", document.getElementsByName("User")[0].value);
+	user.set("password", document.getElementsByName("Pass")[0].value);
+	  
+	user.signUp(null, {
+	  success: function(user) {
+	    // Hooray! Let them use the app now.
+	    location.href='home.html';
+	  },
+	  error: function(user, error) {
+	    // Show the error message somewhere and let the user try again.
+	    alert("signUp Error: " + error.code + " " + error.message);
+	  }
+	});
+};
+
+// //Facebook code
+// function statusChangeCallback(response) {
+// 	console.log('statusChangeCallback');
+// 	console.log(response);
+// 	// The response object is returned with a status field that lets the
+// 	// app know the current login status of the person.
+// 	// Full docs on the response object can be found in the documentation
+// 	// for FB.getLoginStatus().
+// 	if (response.status === 'connected') {
+// 	  // Logged into your app and Facebook.
+// 	  testAPI();
+// 	} else if (response.status === 'not_authorized') {
+// 	  // The person is logged into Facebook, but not your app.
+// 	  document.getElementById('status').innerHTML = 'Please log ' +
+// 	    'into this app.';
+// 	} else {
+// 	  // The person is not logged into Facebook, so we're not sure if
+// 	  // they are logged into this app or not.
+// 	  document.getElementById('status').innerHTML = 'Please log ' +
+// 	    'into Facebook.';
+// 	}
+// 	}
+
+// 	  // This function is called when someone finishes with the Login
+// 	  // Button.  See the onlogin handler attached to it in the sample
+// 	  // code below.
+// 	  function checkLoginState() {
+// 	    FB.getLoginStatus(function(response) {
+// 	      statusChangeCallback(response);
+// 	    });
+// 	  }
+
+// 	  // Now that we've initialized the JavaScript SDK, we call 
+// 	  // FB.getLoginStatus().  This function gets the state of the
+// 	  // person visiting this page and can return one of three states to
+// 	  // the callback you provide.  They can be:
+// 	  //
+// 	  // 1. Logged into your app ('connected')
+// 	  // 2. Logged into Facebook, but not your app ('not_authorized')
+// 	  // 3. Not logged into Facebook and can't tell if they are logged into
+// 	  //    your app or not.
+// 	  //
+// 	  // These three cases are handled in the callback function.
+
+
+// 	  // FB.getLoginStatus(function(response) {
+// 		 //    alert("Checking if logged in already");
+// 		 //    statusChangeCallback(response);
+// 		 //    if( response.status === 'connected'){
+// 		 //      alert("Logged in!");
+// 		 //    }
+// 	  // });
+
+
+// 	  // Here we run a very simple test of the Graph API after login is
+// 	  // successful.  See statusChangeCallback() for when this call is made.
+// 	  function testAPI() {
+// 	    console.log('Welcome!  Fetching your information.... ');
+// 	    FB.api('/me', function(response) {
+// 	      console.log('Successful login for: ' + response.name);
+// 	      document.getElementById('status').innerHTML =
+// 	        'Thanks for logging in, ' + response.name + '!';
+// 	    });
+// 	  }
