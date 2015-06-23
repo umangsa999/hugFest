18449c3fcb9026538f2034f940e1192b2c8dc34c
Chris Lee <chrisl@usc.edu>
2015-06-23 00:01:08 -0700

    Added some comments, updatePlayerProfile, and playerFinishGame function.

--- hugFest2/cloud/main.js
+++ hugFest2/cloud/main.js
@@ -14,11 +14,12 @@ Parse.Cloud.define("findFriend", function(request, response) {
 	})
 });
 
+//call this on load for home.html
 Parse.Cloud.define("homeRefresh", function(request, response){
 	var query = Parse.Query(Parse.User);
-	query.find(request.userId,{
+	query.get(request.userId,{
 		success: function(result){
-			var friends = results.get("friends");
+			var friends = results.get("friends");//incomplete
 		},
 		error: function(results){
 			
@@ -26,10 +27,10 @@ Parse.Cloud.define("homeRefresh", function(request, response){
 	});
 });
 
+//call this for getting rules while in game
 Parse.Cloud.define("getRules", function(request, response){
 	var query = Parse.Query("hugGame");
-	query.equalTo("objectId", request.gameId);
-	query.find({
+	query.get(request.gameId,{
 		success: function(result){
 			response.success({rules: result.get("rulesString")});
 		},
@@ -39,6 +40,7 @@ Parse.Cloud.define("getRules", function(request, response){
 	});
 });
 
+//call this for setting rules after create page
 Parse.Cloud.define("setRules", function(request, response){
 	var query = Parse.Query("hugGame");
 	query.get(request.gameId,{
@@ -56,9 +58,10 @@ Parse.Cloud.define("setRules", function(request, response){
 	});
 });
 
-Parse.Cloud.define("updatePlayer", function(request, response){
+//call this to update a player's profile outside a game
+Parse.Cloud.define("updatePlayerProfile", function(request, response){
 	var query = Parse.Query(Parse.User);
-	query.find(request.gameId, {
+	query.get(request.userId, {
 		sucess: function(result){
 			if (request.name)
 				result.set("name", request.name);
@@ -66,18 +69,8 @@ Parse.Cloud.define("updatePlayer", function(request, response){
 				result.set("nickname", request.nickname);
 			if (request.status)
 				result.set("status", request.status);
-			if (request.target)
-				result.set("target", request.target);
-			if (request.currentGame)
-				result.set("currentGame", request.currentGame);
-			if (request.finishGame){
-				result.set("totalGames", result.get("totalGames") + 1);
-				result.set("totalHugs", result.get("totalHugs") + request.finishGame);
-			}
 			if (request.currentLocation)
 				result.set("currentLocation", request.currentLocation);
-			if (request.friends)
-				result.set("friends", request.friends);
 			if (request.currentImage)
 				result.set("currentImage", request.currentImage);
 			
@@ -88,3 +81,22 @@ Parse.Cloud.define("updatePlayer", function(request, response){
 		}
 	});
 });
+
+//call this to update a player's profile during/after a game
+Parse.Cloud.define("playerFinishGame", function(request, response){
+	var query = Parse.Query(Parse.User);
+	query.get(request.userId, {
+		success: function(result){
+			result.set("totalHugs", result.get("totalHugs") + request.currentHugs;
+			result.set("totalGames", result.get("totalGames") + 1);
+			result.set("currentHugs", 0);
+			//change game to none
+			//change target to none
+			response.success();
+		},
+		error: function(error){
+			response.error({err: error});
+		}
+	});
+});
+
