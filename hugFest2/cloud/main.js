eec25f5e202e17cf31d69eb85bd72890ec68b6ab
Chris Lee <chrisl@usc.edu>
2015-06-22 21:48:52 -0700

    Added general functions for server calls.

--- hugFest2/cloud/main.js
+++ hugFest2/cloud/main.js
@@ -13,3 +13,78 @@ Parse.Cloud.define("findFriend", function(request, response) {
 	  }
 	})
 });
+
+Parse.Cloud.define("homeRefresh", function(request, response){
+	var query = Parse.Query(Parse.User);
+	query.find(request.userId,{
+		success: function(result){
+			var friends = results.get("friends");
+		},
+		error: function(results){
+			
+		}
+	});
+});
+
+Parse.Cloud.define("getRules", function(request, response){
+	var query = Parse.Query("hugGame");
+	query.equalTo("objectId", request.gameId);
+	query.find({
+		success: function(result){
+			response.success({rules: result.get("rulesString")});
+		},
+		error: function(error){
+			response.error({err: error});
+		}
+	});
+});
+
+Parse.Cloud.define("setRules", function(request, response){
+	var query = Parse.Query("hugGame");
+	query.get(request.gameId,{
+		success: function(result){
+			result.set("rulesString", request.rules, {
+				error: function(error){
+					response.error({err: error});
+				}
+			});
+			response.success();
+		},
+		error: function(error){
+			response.error({err: error});
+		}
+	});
+});
+
+Parse.Cloud.define("updatePlayer", function(request, response){
+	var query = Parse.Query(Parse.User);
+	query.find(request.gameId, {
+		sucess: function(result){
+			if (request.name)
+				result.set("name", request.name);
+			if (request.nickname)
+				result.set("nickname", request.nickname);
+			if (request.status)
+				result.set("status", request.status);
+			if (request.target)
+				result.set("target", request.target);
+			if (request.currentGame)
+				result.set("currentGame", request.currentGame);
+			if (request.finishGame){
+				result.set("totalGames", result.get("totalGames") + 1);
+				result.set("totalHugs", result.get("totalHugs") + request.finishGame);
+			}
+			if (request.currentLocation)
+				result.set("currentLocation", request.currentLocation);
+			if (request.friends)
+				result.set("friends", request.friends);
+			if (request.currentImage)
+				result.set("currentImage", request.currentImage);
+			
+			response.success();
+		},
+		error: function(error){
+			response.error(err: error);
+		}
+	});
+});
