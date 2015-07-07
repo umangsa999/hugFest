6f4ea914619d9ca2cd7afa9da97d8ba57e94e6a3
Ryan Zhou <ryanzhou7@gmail.com>
2015-07-06 20:01:52 -0700

    Begin working on shared preferences

--- ImportTest/app/src/main/java/com/example/andy/importtest/LoginActivity.java
+++ ImportTest/app/src/main/java/com/example/andy/importtest/LoginActivity.java
@@ -1,23 +1,111 @@
 package com.example.andy.importtest;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends ActionBarActivity {
-    private EditText user;
-    private EditText pass;
+
+    private EditText editTextUser;
+    private EditText editTextPass;
+    private CheckBox checkBoxRemember;
+    private Boolean rememberInfo = false;
+    private Boolean successfulLogin = false;
+    private SharedPreferences prefSettings;
+    private SharedPreferences.Editor prefEditor;
+
+    //need to make this static because we're using anonymous inner class for button listener
+    private static Button buttonLogin;
+
+    public static final String PREFS_FILE = "userPrefFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        user = (EditText)findViewById(R.id.editTextUser);
-        pass = (EditText)findViewById(R.id.editTextPass);
+        //connect widgets to variables
+        editTextUser = (EditText)findViewById(R.id.editTextUser);
+        editTextPass = (EditText)findViewById(R.id.editTextPass);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        //get saved settings
+        prefSettings = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
+
+        //code for what happens when checkBox is clicked
+        checkBoxRemember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( rememberInfo ){
+                    checkBoxRemember.setChecked(true);
+                    rememberInfo = true;
+
+                    //User wants password to be saved
+                    prefEditor.putBoolean( PREFS_FILE, true);
+
+                }
+                else{
+                    checkBoxRemember.setChecked(false);
+                    rememberInfo = false;
+                }
+            }
+        });
+
+        //set this code to execute when button is clicked
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                rememberInfo = prefSettings.getBoolean("remember", false);
+
+                //first check if the user has logged in before & wanted info remembered
+                if( rememberInfo && successfulLogin  ){
+
+                    //set the user and pass
+                    String user = prefSettings.getString("user", "");
+
+                }
+
+                //new user/never logged in before/didn't save
+                else{
+                    //get the current text from widget and convert to string
+                    String user = editTextUser.getText().toString();
+                    String pass = editTextPass.getText().toString();
+
+                    //TODO -- more comprehensive login check
+                    //check user has entered anything
+                    if( user.length() == 0 || pass.length() == 0 ){
+
+                        //Alert the user
+                        Toast.makeText(
+                                getApplicationContext(), "Please enter valid username/password", Toast.LENGTH_SHORT
+                        ).show();
+                    }
+
+                    //valid user/pass entered
+                    else {
+                        //TODO -- check if username already exists/correct password with database
+                        //TODO -- encrypt password then send
+
+                        //Send info to the database & save in shared preferences
+                        successfulLogin = true;
+                        prefEditor.putBoolean( PREFS_FILE ,successfulLogin);
+
+                    }
+                }
+
+
+            }
+        });
+
     }
 
     @Override
