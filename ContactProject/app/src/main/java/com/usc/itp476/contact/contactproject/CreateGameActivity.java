8cb187594899a194f42d3d9b250d4b302f70f99d
Chris Lee <chrisl@usc.edu>
2015-07-18 14:45:11 -0700

    Enlarged Tab text. Created the structure for the CreateGameActiity.

---
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/CreateGameActivity.java
@@ -0,0 +1,57 @@
+package com.usc.itp476.contact.contactproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CreateGameActivity extends Activity {
+    private Button btnCreate;
+    private TextView txvwMax;
+    private SeekBar skbrMax;
+    private ListView lsvwInvite;
+    private int maxPoints = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_game);
+
+        Intent i = getIntent();
+
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        txvwMax = (TextView) findViewById(R.id.txvwMax);
+        skbrMax = (SeekBar) findViewById(R.id.skbrMax);
+        lsvwInvite = (ListView) findViewById(R.id.lsvwInvite);
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Move to Ingame", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        skbrMax.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                maxPoints = 1 + progress;
+                txvwMax.setText(String.valueOf(maxPoints));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+}
--- ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/slidetab/fragments/GameFragment.java
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/slidetab/fragments/GameFragment.java
@@ -1,5 +1,6 @@
 package com.usc.itp476.contact.contactproject.slidetab.fragments;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.content.Context;
@@ -8,8 +9,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,20 +17,20 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.usc.itp476.contact.contactproject.CreateGameActivity;
 import com.usc.itp476.contact.contactproject.R;
 
 public class GameFragment extends Fragment implements OnMapReadyCallback{
     private GoogleMap map;
     private boolean gameClicked = false;
-    private ImageButton btnCreate;
+    private ImageButton btnGame;
     private LatLng myLoc;
     private SupportMapFragment mapFragment;
     private View rootView;
-    private Circle radiusCircle;
+    private Circle radiusCircle = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -40,9 +39,8 @@ public class GameFragment extends Fragment implements OnMapReadyCallback{
 
         rootView = (View) inflater.inflate(
                     R.layout.activity_home, container, false);
-        Log.wtf(this.getClass().getSimpleName(), "onCreateView container's children count: " + container.getChildCount());
-        //if (savedInstanceState == null) {
-        btnCreate = (ImageButton) rootView.findViewById(R.id.btnCreate);
+
+        btnGame = (ImageButton) rootView.findViewById(R.id.btnGame);
         mapFragment = SupportMapFragment.newInstance();
         FragmentTransaction fragmentTransaction =
                 getChildFragmentManager().beginTransaction();
@@ -71,16 +69,18 @@ public class GameFragment extends Fragment implements OnMapReadyCallback{
         LocationManager LocMan =
                 (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         Location loc = LocMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        LatLng temp = new LatLng(loc.getLatitude(), loc.getLongitude());
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(temp, 15));
 
-        CircleOptions circleOptions = new CircleOptions()
-                .center(new LatLng(loc.getLatitude(), loc.getLongitude()))
-                .radius(700).fillColor(Color.argb(128, 0, 128, 128))
-                .strokeWidth(5).strokeColor(Color.argb(128, 0, 128, 128));
-        radiusCircle = map.addCircle(circleOptions);
+        if (loc != null) {
+            LatLng temp = new LatLng(loc.getLatitude(), loc.getLongitude());
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(temp, 15));
 
-        btnCreate.bringToFront();
+            CircleOptions circleOptions = new CircleOptions()
+                    .center(new LatLng(loc.getLatitude(), loc.getLongitude()))
+                    .radius(700).fillColor(Color.argb(128, 0, 128, 128))
+                    .strokeWidth(5).strokeColor(Color.argb(128, 0, 128, 128));
+            radiusCircle = map.addCircle(circleOptions);
+        }
+        btnGame.bringToFront();
         rootView.requestLayout();
         rootView.invalidate();
     }
@@ -91,15 +91,17 @@ public class GameFragment extends Fragment implements OnMapReadyCallback{
     }
 
     private void setCreateListener(){
-        btnCreate.setOnClickListener(new View.OnClickListener() {
+        btnGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gameClicked = !gameClicked;
+                /*gameClicked = !gameClicked;
                 if (gameClicked) {
-                    btnCreate.setBackgroundResource(R.mipmap.ic_join);
-                }else{
-                    btnCreate.setBackgroundResource(R.mipmap.ic_create);
-                }
+                    btnGame.setBackgroundResource(R.mipmap.ic_join);
+                } else {
+                    btnGame.setBackgroundResource(R.mipmap.ic_create);
+                }*/
+                Intent i = new Intent(GameFragment.this.getActivity().getApplicationContext(), CreateGameActivity.class);
+                startActivity(i);
             }
         });
     }
@@ -112,6 +114,16 @@ public class GameFragment extends Fragment implements OnMapReadyCallback{
             public void onLocationChanged(Location location) {
                 myLoc = new LatLng(location.getLatitude(), location.getLongitude());
                 map.moveCamera(CameraUpdateFactory.newLatLng(myLoc));
+
+                if (radiusCircle != null){
+                    radiusCircle.setCenter(myLoc);
+                }else {
+                    CircleOptions circleOptions = new CircleOptions()
+                            .center(myLoc)
+                            .radius(700).fillColor(Color.argb(128, 0, 128, 128))
+                            .strokeWidth(5).strokeColor(Color.argb(128, 0, 128, 128));
+                    radiusCircle = map.addCircle(circleOptions);
+                }
             }
 
             @Override
