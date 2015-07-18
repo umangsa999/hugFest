ccb9637c6cfb2335cf3551fb199d38fde17529e9
Chris Lee <chrisl@usc.edu>
2015-07-17 17:44:15 -0700

    Added a Home Activity which displays a map of your current location. It will attempt to follow you.

--- ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/StartActivity.java
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/StartActivity.java
@@ -1,13 +1,32 @@
 package com.usc.itp476.contact.contactproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class StartActivity extends Activity {
+    private Button btnStart;
+    private EditText edtxFirst;
+    private EditText edtxLast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        edtxFirst = (EditText) findViewById(R.id.edtxFirst);
+        edtxLast = (EditText) findViewById(R.id.edtxLast);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
