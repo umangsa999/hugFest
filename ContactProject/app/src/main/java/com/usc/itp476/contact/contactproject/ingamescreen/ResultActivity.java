3fd226434136b2db2865dbd7bf088b8422bcd4bd
Chris Lee <chrisl@usc.edu>
2015-07-20 18:12:47 -0700

    Created simplistic UI for ResultActivity.

--- ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/ingamescreen/ResultActivity.java
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/ingamescreen/ResultActivity.java
@@ -2,14 +2,28 @@ package com.usc.itp476.contact.contactproject.ingamescreen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.usc.itp476.contact.contactproject.R;
 
 public class ResultActivity extends Activity {
+    private ImageView winnerImage, leftImage, middleImage, rightImage;
+    private TextView winnerName, leftName, middleName, rightName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        winnerImage = (ImageView) findViewById(R.id.resultsWinnerImage);
+        leftImage = (ImageView) findViewById(R.id.resultsLeftImage);
+        middleImage = (ImageView) findViewById(R.id.resultsMiddleImage);
+        rightImage = (ImageView) findViewById(R.id.resultsRightImage);
+
+        winnerName = (TextView) findViewById(R.id.resultsWinnerName);
+        leftName = (TextView) findViewById(R.id.resultsLeftName);
+        middleName = (TextView) findViewById(R.id.resultsMiddleName);
+        rightName = (TextView) findViewById(R.id.resultsRightName);
     }
 }
