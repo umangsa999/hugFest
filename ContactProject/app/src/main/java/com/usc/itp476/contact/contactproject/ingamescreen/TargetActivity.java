757ca039829509a96540006a5e46b6107f2a392a
Ryan Zhou <ryanzhou7@gmail.com>
2015-07-20 20:39:08 -0700

    Mostly working take camera code extra that not sure if photo loaded

--- ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/ingamescreen/TargetActivity.java
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/ingamescreen/TargetActivity.java
@@ -2,7 +2,14 @@ package com.usc.itp476.contact.contactproject.ingamescreen;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.camera2.CameraManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,17 +18,28 @@ import android.widget.Toast;
 
 import com.usc.itp476.contact.contactproject.R;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class TargetActivity extends Activity {
     public static final String MAXPOINTS = "com.usc.itp476.contact.contactproject";
     private TextView txvwCurrentPoints;
     private TextView txvwMaxPoints;
+    private ImageView mImageView;
     private ImageView imvwTarget;
     private int max;
     private int current = 0;
     private long backPressedTime = 0;
     private final long TIME_INTERVAL = 2000;
     private Toast backToast;
+    private CameraManager manager;
+    private Button buttonTakePicture;
+    private String mCurrentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +59,99 @@ public class TargetActivity extends Activity {
             }
         });
 
+        mImageView = (ImageView) findViewById(R.id.imageViewResult);
         txvwCurrentPoints = (TextView) findViewById(R.id.txvwPoints);
         txvwMaxPoints = (TextView) findViewById(R.id.txvwMaxScore);
         imvwTarget = (ImageView) findViewById(R.id.imvwTarget);
+        buttonTakePicture = (Button) findViewById(R.id.buttonTakePicture);
+
+        buttonTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
         setPoints();
         backToast = Toast.makeText(getApplicationContext(),
                 "Press back again to leave game.",
                 Toast.LENGTH_SHORT);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //V only geta thumbnail
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            //setPic();;
+            Uri selectedImage = data.getData();
+            Bitmap mBitmap = null;
+            
+            mImageView.setImageBitmap(mBitmap);
+        }
+    }
+
+    //use this for lower memory (probably need for gridview friends)
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        mImageView.setImageBitmap(bitmap);
+        imvwTarget.setVisibility(View.GONE);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.wtf("TargetActivity", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
     }
 
     private void increasePoints(){
