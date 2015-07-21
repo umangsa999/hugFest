152e03556e228a1162674485a05de1449b74de97
Chris Lee <chrisl@usc.edu>
2015-07-20 19:25:40 -0700

    Started SharedPreferences.

--- ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/StartActivity.java
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/StartActivity.java
@@ -2,6 +2,7 @@ package com.usc.itp476.contact.contactproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import android.widget.EditText;
 import com.usc.itp476.contact.contactproject.slidetab.AllTabActivity;
 
 public class StartActivity extends Activity {
+    private final String PREFFILE = "com.usc.itp476.contact.contactproject.StartActivity.PREFFILE";
+    private final String USERID = "com.usc.itp476.contact.contactproject.StartActivity.USERID";
     private Button btnStart;
     private EditText edtxFirst;
     private EditText edtxLast;
@@ -19,6 +22,13 @@ public class StartActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFFILE, MODE_PRIVATE);
+        String id = sharedPreferences.getString(USERID, null);
+
+        if (id != null){
+            goToHome();
+        }
+
         btnStart = (Button) findViewById(R.id.btnStart);
         edtxFirst = (EditText) findViewById(R.id.edtxFirst);
         edtxLast = (EditText) findViewById(R.id.edtxLast);
@@ -26,9 +36,13 @@ public class StartActivity extends Activity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), AllTabActivity.class);
-                startActivity(i);
+                goToHome();
             }
         });
+    }
+
+    private void goToHome(){
+        Intent i = new Intent(getApplicationContext(), AllTabActivity.class);
+        startActivity(i);
     }
 }
