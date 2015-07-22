8ec69dabf1811dc3e93fbc82eec1ea82b7c51a79
Chris Lee <chrisl@usc.edu>
2015-07-21 20:00:46 -0700

    Created ContactApplication which extends Application. This is our app. Here, we initialize Parse.

---
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/ContactApplication.java
@@ -0,0 +1,26 @@
+package com.usc.itp476.contact.contactproject;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.usc.itp476.contact.contactproject.POJO.GameMarker;
+
+public class ContactApplication extends Application {
+    private static ContactApplication singleton;
+
+    public ContactApplication getSingleton(){
+        return singleton;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        singleton = this;
+        ParseObject.registerSubclass(GameMarker.class);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this,
+                "ellChjDHP7hNM4CBQLHrBNWzDMoOzElwUgy3MpEc",
+                "aXSv9sdHcVcnjSIaqy8KuymGh16K5I53MiWXGgnN");
+    }
+}
