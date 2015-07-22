353460ca17a6c4b127ad322db2eab497be46ec31
Ryan Zhou <ryanzhou7@gmail.com>
2015-07-22 02:34:00 -0700

    Fully functional facebook login

--- ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/StartActivity.java
+++ ContactProject/app/src/main/java/com/usc/itp476/contact/contactproject/StartActivity.java
@@ -1,173 +1,235 @@
 package com.usc.itp476.contact.contactproject;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
-import com.parse.Parse;
-import com.parse.ParseFacebookUtils;
-import com.parse.ParseUser;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.usc.itp476.contact.contactproject.POJO.GameData;
 import com.usc.itp476.contact.contactproject.slidetab.AllTabActivity;
 
+import java.security.MessageDigest;
+import java.util.Arrays;
 import java.util.List;
 
-public class StartActivity extends Activity {
+public class StartActivity extends FragmentActivity {
     final String TAG = this.getClass().getSimpleName();
     private Button btnStart;
     private EditText edtxFirst;
     private EditText edtxLast;
+    final List<String> permissions = Arrays.asList("public_profile", "user_friends");
+    private LoginButton loginButton;
+    private CallbackManager mCallbackManager;
+    private FacebookCallback<LoginResult> mCallback;
 
     @Override
     protected void onPause() {
         super.onPause();
-
+        SharedPreferences sharedPreferences = getSharedPreferences(GameData.PREFFILE, Context.MODE_PRIVATE);
         // Logs 'app deactivate' App Event.
-        AppEventsLogger.deactivateApp(this);
+        AppEventsLogger.deactivateApp(StartActivity.this);
     }
 
+    //Causes app to crash
     @Override
     protected void onResume() {
         super.onResume();
-
         // Logs 'install' and 'app activate' App Events.
-        AppEventsLogger.activateApp(this);
+        AppEventsLogger.activateApp(StartActivity.this);
+    }
+
+    public static String printKeyHash(Activity context) {
+        PackageInfo packageInfo;
+        String key = null;
+        try {
+            //getting application package name, as defined in manifest
+            String packageName = context.getApplicationContext().getPackageName();
+
+            //Retriving package info
+            packageInfo = context.getPackageManager().getPackageInfo(packageName,
+                    PackageManager.GET_SIGNATURES);
+            Log.e("Package Name=", context.getApplicationContext().getPackageName());
+            for (Signature signature : packageInfo.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                key = new String(Base64.encode(md.digest(), 0));
+
+                // String key = new String(Base64.encodeBytes(md.digest()));
+                Log.e("Key Hash=", key);
+            }
+        } catch (Exception e1) {
+            Log.e("Name not found", e1.toString());
+        }
+        return key;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_start);
 
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "ellChjDHP7hNM4CBQLHrBNWzDMoOzElwUgy3MpEc", "aXSv9sdHcVcnjSIaqy8KuymGh16K5I53MiWXGgnN");
-        ParseFacebookUtils.initialize(getApplicationContext());
-
-
-        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        final List pkgAppsList = getPackageManager().queryIntentActivities(mainIntent, 0);
-
-        for (Object obj : pkgAppsList) {
-            ResolveInfo resolveInfo = (ResolveInfo) obj;
-            PackageInfo packageInfo = null;
-            try {
-                packageInfo = getPackageManager().getPackageInfo(resolveInfo.activityInfo.packageName, PackageManager.GET_PERMISSIONS);
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            String[] permissions = packageInfo.requestedPermissions;
-            for( String i : permissions){
-                Log.wtf(TAG, i );
-            }
-        }
-
-
-
-        SharedPreferences sharedPreferences = getSharedPreferences(GameData.PREFFILE, MODE_PRIVATE);
-        String id = sharedPreferences.getString(GameData.USER_ID, null);
-
-        //the user has already logged in before
-        if (id != null){
-            goToHome();
-        }
-
-        btnStart = (Button) findViewById(R.id.btnStart);
-        edtxFirst = (EditText) findViewById(R.id.edtxFirst);
-        edtxLast = (EditText) findViewById(R.id.edtxLast);
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
+        mCallback = new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View v) {
-
-                ParseUser user = new ParseUser();
-                user.setUsername("my name");
-                user.setPassword("my pass");
-                user.setEmail("email@example.com");
-
-                // other fields can be set just like with ParseObject
-                user.put("phone", "650-555-0000");
-
-                ;
-                //loggin in user
-//                ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
-//                    @Override
-//                    public void done(ParseUser user, ParseException err) {
-//                        if (user == null) {
-//                            Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
-//                        } else if (user.isNew()) {
-//                            Log.d("MyApp", "User signed up and logged in through Facebook!");
-//                        } else {
-//                            Log.d("MyApp", "User logged in through Facebook!");
-//                        }
-//                    }
-//                });
-
-//                user.signUpInBackground(new SignUpCallback() {
-//                    public void done(ParseException e) {
-//                        if (e == null) {
-//                            // Hooray! Let them use the app now.
-//                        } else {
-//                            // Sign up didn't succeed. Look at the ParseException
-//                            // to figure out what went wrong
-//                        }
-//                    }
-//                });
-
-                //name is incompatible
-                if (edtxFirst.getText().length() == 0 || edtxLast.getText().length() == 0){
-//                    Toast.makeText(getApplicationContext(),
-//                            "Please enter a valid name.",
-//                            Toast.LENGTH_SHORT).show();
-//                    return;
-                    Toast.makeText(getApplicationContext(),
-                            "Assuming debugging.",
-                            Toast.LENGTH_SHORT).show();
+            public void onSuccess(LoginResult loginResult) {
+                Log.wtf(TAG, "success");
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                if( profile != null){
+                    //profile.
                 }
-
-                //TODO Make so that user can continue from last time as opposed to always resetting
-                //save a working name
-                SharedPreferences sharedPreferences =
-                        getSharedPreferences(GameData.PREFFILE, MODE_PRIVATE);
-                SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
-
-                sharedPrefEditor.putString(GameData.FULL_NAME,
-                        edtxFirst.getText().toString() + " " + edtxLast.getText().toString());
-
-                //add a random total hugs for now
-                //TODO set to 0
-                sharedPrefEditor.putInt(GameData.TOTAL_HUGS, (int)(Math.random() * 200));
-
-                //TODO add ID here
-
-                //save the user's name asynchronously
-                sharedPrefEditor.apply();
-
-                goToHome();
             }
-        });
-    }
 
+            @Override
+            public void onCancel() {
+                Log.wtf(TAG, "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.wtf(TAG, "error");
+            }
+        };
+
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        //loginButton.setFragment(this);
+        loginButton.setReadPermissions(permissions);
+        loginButton.registerCallback( mCallbackManager, mCallback );
+
+        Log.wtf(TAG, printKeyHash(this));
+//
+//        // Enable Local Datastore.
+//        Parse.enableLocalDatastore(this);
+//        Parse.initialize(this, "ellChjDHP7hNM4CBQLHrBNWzDMoOzElwUgy3MpEc", "aXSv9sdHcVcnjSIaqy8KuymGh16K5I53MiWXGgnN");
+//        //String appId = getString(R.string.app_id);
+//        ParseFacebookUtils.initialize(this);
+//
+//        loginButton = (LoginButton) findViewById(R.id.login_button);
+//
+//        // Check if there is current user info
+//        if (ParseUser.getCurrentUser() != null) {
+//            // Start an intent for the logged in activity
+//            Log.d("F8Debug", "onCreate, got user,  "
+//                    + ParseUser.getCurrentUser().getUsername());
+//            startActivity(new Intent(this, AllTabActivity.class));
+//        } else {
+//            // Start and intent for the logged out activity
+//            Log.d("F8Debug", "onCreate, no user");
+//            //startActivity(new Intent(this, SignInActivity.class));
+//        }
+//        final List<String> permissions = Arrays.asList("public_profile");
+//        permissions.add("user_friends");
+//
+//        SharedPreferences sharedPreferences = getSharedPreferences(GameData.PREFFILE, MODE_PRIVATE);
+//        String id = sharedPreferences.getString(GameData.USER_ID, null);
+//
+//        //the user has already logged in before
+//        if (id != null){
+//            goToHome();
+//        }
+//
+//        btnStart = (Button) findViewById(R.id.btnStart);
+//        edtxFirst = (EditText) findViewById(R.id.edtxFirst);
+//        edtxLast = (EditText) findViewById(R.id.edtxLast);
+//
+//
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                ParseUser user = new ParseUser();
+//                user.setUsername("my name");
+//                user.setPassword("my pass");
+//                user.setEmail("email@example.com");
+//
+//                // other fields can be set just like with ParseObject
+//                user.put("phone", "650-555-0000");
+//
+//                //log them in automatically
+//                ParseFacebookUtils.logInWithReadPermissionsInBackground(
+//                        StartActivity.this, permissions, new LogInCallback() {
+//                            @Override
+//                            public void done(ParseUser user, ParseException err) {
+//                        if (user == null) {
+//                            Log.wtf("MyApp", "Uh oh. The user cancelled the Facebook login.");
+//                        } else if (user.isNew()) {
+//                            Log.wtf("MyApp", "User signed up and logged in through Facebook!");
+//                        } else {
+//                            Log.wtf("MyApp", "User logged in through Facebook!");
+//                        }
+//                    }
+//                });
+//
+////                user.signUpInBackground(new SignUpCallback() {
+////                    public void done(ParseException e) {
+////                        if (e == null) {
+////                            // Hooray! Let them use the app now.
+////                        } else {
+////                            // Sign up didn't succeed. Look at the ParseException
+////                            // to figure out what went wrong
+////                        }
+////                    }
+////                });
+//
+//                //name is incompatible
+//                if (edtxFirst.getText().length() == 0 || edtxLast.getText().length() == 0){
+////                    Toast.makeText(getApplicationContext(),
+////                            "Please enter a valid name.",
+////                            Toast.LENGTH_SHORT).show();
+////                    return;
+//                    Toast.makeText(getApplicationContext(),
+//                            "Assuming debugging.",
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//
+//                //TODO Make so that user can continue from last time as opposed to always resetting
+//                //save a working name
+//                SharedPreferences sharedPreferences =
+//                        getSharedPreferences(GameData.PREFFILE, MODE_PRIVATE);
+//                SharedPreferences.Editor sharedPrefEditor = sharedPreferences.edit();
+//
+//                sharedPrefEditor.putString(GameData.FULL_NAME,
+//                        edtxFirst.getText().toString() + " " + edtxLast.getText().toString());
+//
+//                //add a random total hugs for now
+//                //TODO set to 0
+//                sharedPrefEditor.putInt(GameData.TOTAL_HUGS, (int)(Math.random() * 200));
+//
+//                //TODO add ID here
+//
+//                //save the user's name asynchronously
+//                sharedPrefEditor.apply();
+//
+//                //goToHome();
+//            }
+//        });
+    }
 
     //Called when facebook is done
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        ParseFacebookUtils.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult( requestCode, resultCode, data);
     }
-
     private void goToHome(){
         Intent i = new Intent(getApplicationContext(), AllTabActivity.class);
         startActivity(i);
